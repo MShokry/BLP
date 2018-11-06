761094b0058069553ce6c2809c62592a1450f41b
Mahmoud Shokry <msn4eg@gmail.com>
2018-11-06 15:04:24 +0100

    Add files via upload

---
+++ Blb.ino
@@ -0,0 +1,189 @@
+#include "DHTesp.h"
+
+DHTesp dht;
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// Replace with your network credentials
+
+const char* ssid = "Amrs-Home";
+const char* password = "01116677005AA";
+
+
+const uint16_t kIrLed = 14;  
+IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
+// an IR detector/demodulator
+uint16_t RECV_PIN = 5;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+const int MQ =  12;      // 
+
+const int RCWLp =  4;      // 
+int RCWLold,RCWL;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(RCWL, INPUT);
+  pinMode(MQ, OUTPUT);
+
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  OTA();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
+  irsend.begin();
+    irrecv.enableIRIn();  // Start the receiver
+  dht.setup(2, DHTesp::DHT22); // Connect DHT sensor to 
+  RCWLold = digitalRead(RCWLp);
+}
+
+void loop()
+{
+  delay(dht.getMinimumSamplingPeriod());
+  ArduinoOTA.handle();
+  if (irrecv.decode(&results)) {
+    dump(&results);
+    Serial.println("DEPRECATED: Please use IRrecvDumpV2.ino instead!");
+    irrecv.resume();  // Receive the next value
+  }
+  
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+  Serial.print(dht.getStatusString());
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.print(dht.toFahrenheit(temperature), 1);
+  Serial.print("\t\t");
+  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
+  Serial.print("\t\t");
+  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
+  Serial.println("NEC");
+  irsend.sendNEC(0x00FFE01FUL, 32);
+  
+  RCWL = digitalRead(RCWLp);
+  if (RCWL != RCWLold) {
+    Serial.println("#####Motion Detected#####");
+  }
+  RCWLold=  RCWL;
+
+
+  // print the results to the Serial Monitor:
+  digitalWrite(MQ, HIGH);
+  delay(50);
+  int sensorValue = analogRead(0);
+  Serial.print("MQ2 = ");
+  Serial.print(sensorValue);
+  digitalWrite(MQ, LOW);
+  delay(50);
+  sensorValue = analogRead(0);
+  Serial.print("MP = ");
+  Serial.print(sensorValue);
+  
+}
+
+void dump(decode_results *results) {
+  // Dumps out the decode_results structure.
+  // Call this after IRrecv::decode()
+  uint16_t count = results->rawlen;
+  if (results->decode_type == UNKNOWN) {
+    Serial.print("Unknown encoding: ");
+  } else if (results->decode_type == NEC) {
+    Serial.print("Decoded NEC: ");
+  } else if (results->decode_type == SONY) {
+    Serial.print("Decoded SONY: ");
+  } else if (results->decode_type == RC5) {
+    Serial.print("Decoded RC5: ");
+  } else if (results->decode_type == RC5X) {
+    Serial.print("Decoded RC5X: ");
+  } else if (results->decode_type == RC6) {
+    Serial.print("Decoded RC6: ");
+  } else if (results->decode_type == RCMM) {
+    Serial.print("Decoded RCMM: ");
+  } else if (results->decode_type == PANASONIC) {
+    Serial.print("Decoded PANASONIC - Address: ");
+    Serial.print(results->address, HEX);
+    Serial.print(" Value: ");
+  } else if (results->decode_type == LG) {
+    Serial.print("Decoded LG: ");
+  } else if (results->decode_type == JVC) {
+    Serial.print("Decoded JVC: ");
+  } else if (results->decode_type == AIWA_RC_T501) {
+    Serial.print("Decoded AIWA RC T501: ");
+  } else if (results->decode_type == WHYNTER) {
+    Serial.print("Decoded Whynter: ");
+  } else if (results->decode_type == NIKAI) {
+    Serial.print("Decoded Nikai: ");
+  }
+  serialPrintUint64(results->value, 16);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+  Serial.print("Raw (");
+  Serial.print(count, DEC);
+  Serial.print("): {");
+
+  for (uint16_t i = 1; i < count; i++) {
+    if (i % 100 == 0)
+      yield();  // Preemptive yield every 100th entry to feed the WDT.
+    if (i & 1) {
+      Serial.print(results->rawbuf[i] * kRawTick, DEC);
+    } else {
+      Serial.print(", ");
+      Serial.print((uint32_t) results->rawbuf[i] * kRawTick, DEC);
+    }
+  }
+  Serial.println("};");
+}
+
+void OTA(){
+  // Port defaults to 8266
+   ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+   ArduinoOTA.setHostname("myesp8266");
+  
+   ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+
